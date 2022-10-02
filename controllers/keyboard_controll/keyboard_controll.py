90cfea1f0f512a3da09ac975eb04e7d30c128437
Shiva <shiva.n404@gmail.com>
2022-10-01 18:11:12 -0700

    Camera integration

--- controllers/keyboard_controll/keyboard_controll.py
+++ controllers/keyboard_controll/keyboard_controll.py
@@ -2,7 +2,7 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot, Keyboard, GPS
+from controller import Robot, Keyboard, GPS, InertialUnit, Camera
 
 TIME_STEP = 64
 robot = Robot()
@@ -24,8 +24,23 @@ keyboard.enable(TIME_STEP)
 leftSpeed = 0.0
 rightSpeed = 0.0
 
-gp = robot.getGPS("global")
+gp = robot.getDevice("global")
 gp.enable(TIME_STEP)
+
+imu = robot.getDevice("imu")
+imu.enable(TIME_STEP)
+
+lr = robot.getDevice('linear')
+
+rm = robot.getDevice('rm')
+
+cam = robot.getDevice('cam')
+cam.enable(TIME_STEP)
+
+
+linear = 0
+rotation=0
+
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(TIME_STEP) != -1:
@@ -47,11 +62,26 @@ while robot.step(TIME_STEP) != -1:
         leftSpeed = 0.0
         rightSpeed=0.0
     
-    print(f"Reading from right_sensor={ds[0].getValue()} lef_sensor={ds[1].getValue()}")
-    print(f"gps X={gp.getValues()[0]} Y={gp.getValues()[1]} Z={gp.getValues()[2]}")
+    # print(f"Reading from right_sensor={ds[0].getValue()} lef_sensor={ds[1].getValue()}")
+    # print(f"gps X={gp.getValues()[0]} Y={gp.getValues()[1]} Z={gp.getValues()[2]}")
+    # print(f"imuX={imu.getRollPitchYaw()[0]} Y={imu.getRollPitchYaw()[1]} Z={imu.getRollPitchYaw()[2]}")
+    
     wheels[0].setVelocity(leftSpeed)
     wheels[1].setVelocity(rightSpeed)
     wheels[2].setVelocity(leftSpeed)
     wheels[3].setVelocity(rightSpeed)
     
+    if key == 87 and linear < 0: # W key
+        linear += 0.005;
+    elif key == 83 and linear > -0.2: # S key
+        linear += -0.005;
+        
+    lr.setPosition(linear)
+    
+    if key == 65 and rotation < 1.57: # A key
+        rotation += 0.05;
+    elif key == 68 > -1.57 : # D key
+        rotation += -0.05;
+        
+    rm.setPosition(rotation)
  
